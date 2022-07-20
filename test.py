27fe20fd92b0acdeb2a5734c7b40d629947ce9b8
alexandrkardash <alexandr.kardash@coinspaid.com>
2022-07-20 06:16:59 +0300

    remove national

--- test.py
+++ test.py
@@ -1,9 +1,35 @@
 import requests
 
-s = requests.Session()
-headers = { 'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9', 'Accept-Language': 'en-US,en;q=0.9,ru;q=0.8', 'Connection': 'keep-alive',
-            'Referer': f'https://service2.diplo.de/rktermin/extern/choose_category.do?locationCode=mins&realmId=231&categoryId=373','Sec-Fetch-Dest': 'document','Sec-Fetch-Mode': 'navigate','Sec-Fetch-Site': 'same-origin','Sec-Fetch-User': '?1','Upgrade-Insecure-Requests': '1','User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.0.0 Safari/537.36','sec-ch-ua': '".Not/A)Brand";v="99", "Google Chrome";v="103", "Chromium";v="103"','sec-ch-ua-mobile': '?0','sec-ch-ua-platform': '"macOS"',
-            }
-s.get('https://service2.diplo.de/rktermin/extern/choose_category.do?locationCode=mins&realmId=231&categoryId=373', headers=headers)
-print(s.cookies)
-print(s.cookies.get_dict()['JSESSIONID'])
+cookies = {
+    'JSESSIONID': 'D03227BC78C567382DC6BF57B21C23E8',
+    'KEKS': 'TERMIN340',
+}
+
+headers = {
+    'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
+    'Accept-Language': 'en-US,en;q=0.9,ru;q=0.8',
+    'Connection': 'keep-alive',
+    # Requests sorts cookies= alphabetically
+    # 'Cookie': 'JSESSIONID=D03227BC78C567382DC6BF57B21C23E8; KEKS=TERMIN340',
+    'Referer': 'https://service2.diplo.de/rktermin/extern/appointment_showDay.do',
+    'Sec-Fetch-Dest': 'document',
+    'Sec-Fetch-Mode': 'navigate',
+    'Sec-Fetch-Site': 'same-origin',
+    'Sec-Fetch-User': '?1',
+    'Upgrade-Insecure-Requests': '1',
+    'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.0.0 Safari/537.36',
+    'sec-ch-ua': '".Not/A)Brand";v="99", "Google Chrome";v="103", "Chromium";v="103"',
+    'sec-ch-ua-mobile': '?0',
+    'sec-ch-ua-platform': '"macOS"',
+}
+
+params = {
+    'locationCode': 'mins',
+    'realmId': '231',
+    'categoryId': '375',
+    'dateStr': '18.08.2022',
+    'openingPeriodId': '29296',
+}
+
+response = requests.get('https://service2.diplo.de/rktermin/extern/appointment_showForm.do', params=params, cookies=cookies, headers=headers)
+print(response.text)
