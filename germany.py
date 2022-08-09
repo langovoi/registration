097c3c5c61c24a89ab71ee097eb3b9b0b75a122f
alexandrkardash <alexandr.kardash@coinspaid.com>
2022-08-09 12:51:50 +0300

    update open page

--- germany.py
+++ germany.py
@@ -37,6 +37,7 @@ class Germany():
 
     def get_session_id(self, url):
         self.s = requests.Session()
+        self.s.cookies.clear()
         headers = { 'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9', 'Accept-Language': 'en-US,en;q=0.9,ru;q=0.8', 'Connection': 'keep-alive', 'Referer': f'{url}','Sec-Fetch-Dest': 'document','Sec-Fetch-Mode': 'navigate','Sec-Fetch-Site': 'same-origin','Sec-Fetch-User': '?1','Upgrade-Insecure-Requests': '1','User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.0.0 Safari/537.36','sec-ch-ua': '".Not/A)Brand";v="99", "Google Chrome";v="103", "Chromium";v="103"','sec-ch-ua-mobile': '?0','sec-ch-ua-platform': '"macOS"',}
         r = self.s.get(url, headers=headers)
         if 'JSESSIONID' in self.s.cookies.get_dict():
