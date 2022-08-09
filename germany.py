287f85c9767ac953492253aa611d8efb3f4400c1
alexandrkardash <alexandr.kardash@coinspaid.com>
2022-08-09 12:44:47 +0300

    update open page

--- germany.py
+++ germany.py
@@ -36,6 +36,7 @@ class Germany():
         self.errors = []
 
     def get_session_id(self, url):
+        self.s = requests.Session()
         headers = { 'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9', 'Accept-Language': 'en-US,en;q=0.9,ru;q=0.8', 'Connection': 'keep-alive', 'Referer': f'{url}','Sec-Fetch-Dest': 'document','Sec-Fetch-Mode': 'navigate','Sec-Fetch-Site': 'same-origin','Sec-Fetch-User': '?1','Upgrade-Insecure-Requests': '1','User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.0.0 Safari/537.36','sec-ch-ua': '".Not/A)Brand";v="99", "Google Chrome";v="103", "Chromium";v="103"','sec-ch-ua-mobile': '?0','sec-ch-ua-platform': '"macOS"',}
         r = self.s.get(url, headers=headers)
         if 'JSESSIONID' in self.s.cookies.get_dict():
@@ -69,7 +70,6 @@ class Germany():
         date_slots = []
         logging.warning('open appointments page')
         html = ''
-        code = 'asdfqw'
         for _ in range(3):
             html = self.open_page('appointments', code=code).text
             if 'Unfortunately' in html:
@@ -91,7 +91,9 @@ class Germany():
                     if code is None: telegram.send_doc('⭕ Ошибка, не смог решить капчу', html)
                 else:
                     telegram.send_doc('⭕ Ошибка, капча не отображается', html)
+                    code = self.open_login_page_get_captcha_code()
                 sleep(60)  # if captcha
+
         else:
             telegram.send_doc(f'⭕ Не разгадал капчу с 3 попыток для категории {self.categories[str(self.category)]}', html)
             raise RuntimeError(f'⭕ Не разгадал капчу с 3 попыток для категории {self.categories[str(self.category)]}')
