bb442616ff1cd6af51ee83368e6cdbd4c7d4000c
alexandrkardash <alexandr.kardash@coinspaid.com>
2022-08-11 17:44:11 +0300

    update workflow

--- utils/captcha.py
+++ utils/captcha.py
@@ -57,7 +57,8 @@ def is_captcha_displayed(html: str):
         try:
             image = image[0]['style'].split("url('")[1].split("')")[0]
             try:
-                return True if len(base64.b64decode(image.split(',')[1])) > 5000 else False
+                base64.b64decode(image.split(',')[1])
+                return True
             except Exception:
                 sleep(120)
                 return False
