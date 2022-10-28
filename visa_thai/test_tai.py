8449dd91a310e8a919d81e01a3f203201657e93a
sergzhig <zse1@bk.ru>
2022-10-29 01:09:39 +0300

    fix hungary : time9 261022 webdriver

--- visa_thai/test_tai.py
+++ visa_thai/test_tai.py
@@ -1,5 +1,6 @@
 import logging
 import os
+import re
 import sys
 from time import sleep
 
@@ -15,6 +16,7 @@ from utils.sim import Sim
 import pyperclip as pc
 from selenium.webdriver.common.alert import Alert
 import requests
+import re 
 
 CURRENT_DIR = os.path.dirname(os.path.abspath(__file__))
 sys.path.append(os.path.dirname(CURRENT_DIR))
@@ -23,20 +25,7 @@ sys.path.append(os.path.dirname(CURRENT_DIR))
 def book_thai(driver):
 
     try:
-        s = requests.Session()
-        code = None
-        latest_code = None
-        # баланс
-        # r=s.get(f'https://simsms.org/priemnik.php?metod=get_balance&service=opt4&apikey=cmLKsSpxYQewPk6xDk7kRmlqRU5drw')
-        # номер
-        r = s.get(
-            f'https://simsms.org/priemnik.php?metod=get_number&country=TH&service=opt19&apikey=cmLKsSpxYQewPk6xDk7kRmlqRU5drw')
-        if r.json()['number']:
-            sim_phone = r.json()['number']
-            sim_id = r.json()['id']
-        logging.warning(sim_phone)
-
-        name="POPOV IVAN"
+        name="POP IVA"
         logging.warning("вход в book")
         driver.find_elements(By.XPATH, '//a[contains(@class,"ui-state-default")]')[0].click()
         logging.warning("выбрали дату")
@@ -47,102 +36,102 @@ def book_thai(driver):
 
 
         for i in range(2):
+            s = requests.Session()
+            code = None
+            latest_code = None
+            # баланс
+            # r=s.get(f'https://simsms.org/priemnik.php?metod=get_balance&service=opt4&apikey=cmLKsSpxYQewPk6xDk7kRmlqRU5drw')
+            # номер
+            r = s.get(
+                f'https://simsms.org/priemnik.php?metod=get_number&country=TH&service=opt19&apikey=cmLKsSpxYQewPk6xDk7kRmlqRU5drw')
+            if r.json()['number']:
+                sim_phone = r.json()['number']
+                sim_id = r.json()['id']
+            logging.warning(sim_phone)
+
             driver.find_element(By.XPATH, '//input[@id="name-input"]').clear()
+            driver.find_element(By.XPATH, '//input[@id="name-input"]').click()
             driver.find_element(By.XPATH, '//input[@id="name-input"]').send_keys(name)
             driver.find_element(By.XPATH, '//div[@class="input-container"]/button[@type="submit" and text()="Done"]').click()
             logging.warning("прописали имя ок")
+
+            driver.find_element(By.XPATH, '//div[@class="selected-dial-code"]').click()
+            driver.find_element(By.XPATH, '//span[@class="country-name" and  contains(text(), "Thailand")]').click()
             phone_field = driver.find_element(By.XPATH, '//input[@class="text-box single-line"]')
             phone_field.clear()
             phone_field.send_keys(sim_phone)
             driver.find_element(By.XPATH, '//div[@class="input-container l-c-my-cust-si--input"]/button').click()
             logging.warning("прописали номер ок ждем код")
-            for i in range(2):
-
-                # sim = Sim('malaysia', 'other')
-                # sim_phone="297982678"
-                # code ="1234"
-                # sim_id, sim_phone = sim.sim_id, sim.sim_phone.replace('+60', '')
-                # logging.warning(sim_phone)
 
 
-                # bl = gsheets.GoogleSheets('vk_blacklist')
-                # black_list = bl.ws.get_all_values()
-                # if [sim_phone] in black_list:
-                #     sim.ban_sim()
-                #     driver.quit()
-                #     return None, None
-                # phone_field.send_keys(Keys.BACKSPACE)
-                # получить номер из 5sim в simphone
-                # code = sim.get_new_code()
-                # simsms sms
-
-                for _ in range(5):
-                    try:
-                        sleep(10)
-                        r2 = s.get(
-                            f'https://simsms.org/priemnik.php?metod=get_sms&country=th&service=opt19&id= {sim_id}&apikey=cmLKsSpxYQewPk6xDk7kRmlqRU5drw')
-                        if r.json()['sms']:
-                            new_code = r.json()['sms']
-                        else:
-                            new_code = None
-                        if new_code != latest_code:
-                            code = new_code
-                            break
-                    except Exception:
-                        pass
-                    sleep(10)
-                sleep(3)
-                if code:
-                    break
-                else:
-                    driver.find_element(By.XPATH, '//a[contains (@href, "https://my.linistry.com/Customer/")]').click()
-
-                #     sim.ban_sim()
-                #
-                # driver.quit()
+            for _ in range(5):
+                try:
+                    s2 = requests.Session()
+                    r2 = s2.get(f'https://simsms.org/priemnik.php?metod=get_sms&country=TH&service=opt19&id={sim_id}&apikey=cmLKsSpxYQewPk6xDk7kRmlqRU5drw')
+                    sleep(5)
+                    if r2.json()['sms']:
+                        new_code_str = r2.json()['sms']
+                        new_code = re.findall(r'\d+', new_code_str)
+                    else:
+                        new_code = None
+                    if new_code != latest_code:
+                        code = new_code
+                        logging.warning(code)
+                        break
+                except Exception as e:
+                    logging.warning(f'Сбой кода: {str(e)}')
+                sleep(5)
+            if code:
+                break
 
             else:
-                raise RuntimeError('otp')
-            driver.find_element(By.XPATH, '//input[@id="Pin"]').clear()
-            driver.find_element(By.XPATH, '//input[@id="Pin"]').send_keys(code)
-            driver.find_element(By.XPATH, '//button[@type="submit" and text()="Done"]').click()
-            # чегото вылетает тут, но логика, чтоб выйти из цикла break с этой датой
-            # if  driver.find_element(By.XPATH, '//li[contains(text(), "Sorry this appointment is already taken"]').is_displayed():
-            #     logging.warning("дата занята")
-            #     raise Exception ("дата недоступна ")
-            if driver.find_element(By.XPATH,
-                                   '//li[text()="Invalid validation code. Please try again!"]').is_displayed():
-
                 driver.find_element(By.XPATH, '//a[contains (@href, "https://my.linistry.com/Customer/")]').click()
 
-            # можно вернуться, выбрать другую дату otp подойдет(новый вводить не нужно)
-            else:
-                try:
-                    #telegram.send_image(driver, f'{name} Зарегестрирован')
-                    #отмена записи кнопкой ниже альтернатива:
-                    # actions = ActionChains(driver)
-                    #
-                    # element = driver.find_element(By.XPATH, '//div[@class="ticket__bottom-cta-decor"]')
-                    # try:
-                    #     actions.move_to_element(element).perform()
-                    # except Exception:
-                    #     sleep(2)
-                    #     actions.move_to_element(element).perform()
-                    # element.click()
 
-                    driver.find_element(By.XPATH, '//button[@id="ticketDropDown"]').click()
-                    driver.find_element(By.XPATH, '//a[contains(@id, "ticket-copy-link")]').click()
-                    driver.switch_to.alert.accept()
-                    link_clipb = pc.paste()
-                    logging.warning(f'Зарегестрирован. Ссылка для{name}: {link_clipb}')
-                    # telegram.send_message(f'Ссылка для{name}: {link_clipb}')
+        driver.find_element(By.XPATH, '//input[@id="Pin"]').clear()
+        driver.find_element(By.XPATH, '//input[@id="Pin"]').send_keys(code)
+        driver.find_element(By.XPATH, '//button[@type="submit" and text()="Done"]').click()
+        try:
 
-                    # !!!отмена реги!!!варнинг!!
-                    # driver.find_element(By.XPATH, '//a[contains(@id, "ticket-signout")]').click()
-                    # driver.switch_to.alert.accept()
-                    # logging.warning(f'рега для {name}')
-                except Exception as exc:
-                    logging.warning(f'Сбой во время отмены: {str(exc)}')
+        # чегото вылетает тут, но логика можно, чтоб выйти из цикла с этой датой
+            if  driver.find_element(By.XPATH, '//li[contains(text(), "Sorry this appointment is already taken"]').is_displayed():
+                logging.warning("дата занята")
+                raise Exception ("дата недоступна ")
+            if driver.find_element(By.XPATH,
+                                  '//li[text()="Invalid validation code. Please try again!"]').is_displayed():
+                driver.find_element(By.XPATH, '//a[contains (@href, "https://my.linistry.com/Customer/")]').click()
+        except:
+            pass
+
+
+        # можно вернуться, выбрать другую дату otp подойдет(новый вводить не нужно)
+
+        try:
+            telegram.send_image(driver, f'{name} Зарегестрирован')
+            #отмена записи кнопкой ниже альтернатива:
+            # actions = ActionChains(driver)
+            #
+            # element = driver.find_element(By.XPATH, '//div[@class="ticket__bottom-cta-decor"]')
+            # try:
+            #     actions.move_to_element(element).perform()
+            # except Exception:
+            #     sleep(2)
+            #     actions.move_to_element(element).perform()
+            # element.click()
+
+            driver.find_element(By.XPATH, '//button[@id="ticketDropDown"]').click()
+            driver.find_element(By.XPATH, '//a[contains(@id, "ticket-copy-link")]').click()
+            driver.switch_to.alert.accept()
+            link_clipb = pc.paste()
+            logging.warning(f'Зарегестрирован. Ссылка для{name}: {link_clipb}')
+            telegram.send_message(f'Ссылка для{name}: {link_clipb}')
+
+            # !!!отмена реги!!!варнинг!!
+            # driver.find_element(By.XPATH, '//a[contains(@id, "ticket-signout")]').click()
+            # driver.switch_to.alert.accept()
+            # logging.warning(f'рега для {name}')
+        except Exception as exc:
+            logging.warning(f'Сбой во время отмены: {str(exc)}')
     except Exception as ex:
         logging.warning(f'Таиланд Неизвестная ошибка при букинге или нет дат: {str(ex)}')
 
